eeaa68014dc3983d92ac9707d3a8796059055685
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-04 23:06:54 +0530

    [Swathi]: Added Calculating Length Program

--- Comparison/Program.cs
+++ Comparison/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LineComparison;
+using LineComparison.LineComparison.Comparison;
+
+namespace LineComparision
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("----------------------------------");
+            //Usecase-1
+            double firstline, secondline;
+            CalculatingLength uC_1Calculate = new CalculatingLength(3, 4, 5, 6);
+            uC_1Calculate.Calculate();
+            Console.WriteLine("----------------------------------");
+        }
+    }
+}
