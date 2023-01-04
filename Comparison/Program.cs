d894e00b4f9fcbd0cae6175ed984212cbd2dc07c
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-04 23:12:34 +0530

    [Swathi]: Added Checking Equality of Lines Program

--- Comparison/Program.cs
+++ Comparison/Program.cs
@@ -14,6 +14,22 @@ namespace LineComparision
             CalculatingLength uC_1Calculate = new CalculatingLength(3, 4, 5, 6);
             uC_1Calculate.Calculate();
             Console.WriteLine("----------------------------------");
+
+            //Usecase-2
+            CheckEquality line = new CheckEquality(5, 6, 7, 8);
+            line.Checking();
+            CheckEquality line1 = new CheckEquality(1, 2, 3, 4);
+            line1.Checking();
+            if (line == line1)
+            {
+                Console.WriteLine("Two lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Two lines are Not equal");
+            }
+            Console.WriteLine("-----------------------------------");
+
         }
     }
 }
